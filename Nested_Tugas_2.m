9c9ff6432ca84ab8063a812533a065718ed73a32
unknown <662023006@student.usw.edu>
2025-04-21 21:24:30 +0700

    praktikum dan tugas

---
+++ Nested_Tugas_2.m
@@ -0,0 +1,16 @@
+%NESTED
+% Input angka
+angka = input('Masukkan sebuah angka: ');
+
+% Menentukan apakah angka tersebut positif, negatif, atau nol
+if angka > 0
+    if angka > 200
+        disp(['Angka ', num2str(angka), ' adalah positif dan lebih dari 200']);
+        else
+        disp(['Angka ', num2str(angka), ' adalah positif dan kurang dari 200']);
+    end
+    elseif angka < 0
+    disp(['Angka ', num2str(angka), ' adalah negatif.']);
+else
+    disp('Angka tersebut adalah nol.');
+end
